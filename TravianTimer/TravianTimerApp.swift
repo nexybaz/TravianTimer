506cbb40b7ac30f4260994ab8892ca4f9f53386d
Marc <marc@freiburghaus.work>
2026-02-11 22:36:56 +0100

    Initial Commit

---
+++ TravianTimer/TravianTimerApp.swift
@@ -0,0 +1,32 @@
+//
+//  TravianTimerApp.swift
+//  TravianTimer
+//
+//  Created by Marc Freiburghaus on 11.02.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TravianTimerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
